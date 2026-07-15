3947d914a3620f8ded1832d9e11c27a36eefd85e
Tesda_Jez <jezreelborlongan7@gmail.com>
2026-07-15 15:40:54 +0800

    Day 2: How to import, create constructors and debug

--- chapter1/Main.java
+++ chapter1/Main.java
@@ -1,4 +1,5 @@
 package chapter1; // This is the package name, it is used to group related classes together
+import java.util.*; //importing the ArrayList class from the java.util package
 
 public class Main{
     public static void main(String[] args){ //psvm is for entry point of the program
@@ -8,6 +9,11 @@ public class Main{
     toy1.price = 4500;
     toy1.quantity = 12;
     System.out.println("Hello, Toy!");
-
+    Random r = new Random();
+    System.out.println(r.nextInt(100));
+    ArrayList al = new ArrayList();
+    System.out.println(al);
+    Date d = new Date();
+    System.out.println(d);
     }
 }
---
+++ chapter1/NamingConflictsLesson.java
@@ -0,0 +1,9 @@
+package otherFolder.a;
+import java.util.Date; 
+
+public class NamingConflictsLesson {
+    Date d1;
+    java.sql.Date d2;
+    //otherFolder.a.SunFlower s1;
+    //otherFolder.a.Makahiya m1;
+}
