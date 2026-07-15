e0e7593392a2b24d3c616bbd65115f70207e787b
Tesda_Jez <jezreelborlongan7@gmail.com>
2026-07-15 16:05:06 +0800

    🐛 fixed: letters conflict & changed the output to Kamusta Mundo!

---
+++ Task1.java
@@ -0,0 +1,5 @@
+class Task1 { // I just changed the class name to Task1 to match the file name given for this task.
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
