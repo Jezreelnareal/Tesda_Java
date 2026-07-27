3c6a5897ed598a98b3e9589377d77636dd4c2f06
Tesda_Jez <jezreelborlongan7@gmail.com>
2026-07-28 03:43:25 +0800

    Task2

---
+++ Task2.java
@@ -0,0 +1,12 @@
+public class Task2 {
+    public static void main(String[] args) {
+        String h = "H",  w = "w", r = "r",  d = "d";
+        int num = 3110;
+        byte zero = 0;
+        int num2 = 1;
+        float dec = 2.0f;
+        boolean bool = true;
+        String output = h + num + " " + w + zero + r + num2 + d + " " + dec + " " + bool;
+        System.out.println(output);
+    }
+}
