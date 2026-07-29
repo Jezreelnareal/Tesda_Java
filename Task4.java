5b334fce7b64cfaa6c962eae478a3b14e85c280a
Tesda_Jez <jezreelborlongan7@gmail.com>
2026-07-29 15:51:27 +0800

    Task4: Palindrome

---
+++ Task4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();// Read the input string from the user
+
+        String reversed = new StringBuilder(input).reverse().toString();// Reverse the input string
+
+        if (input.equals(reversed)) {
+            System.out.println("The input string is a palindrome.");
+        } 
+        else {
+            System.out.println("The input string is not a palindrome.");
+        }
+
+        scanner.close();
+    }
+}
